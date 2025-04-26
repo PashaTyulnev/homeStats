742d583c931258ebe6d63851ac64c63167814452
Pavel Tyulnev <pash_tyul95@mail.ru>
2025-04-26 19:44:21 +0200

    Mobile

--- assets/controllers/main_controller.js
+++ assets/controllers/main_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-
     static targets = [ 'currentDataContainer' ];
+
     connect() {
+        // Aktualisiere die aktuellen Werte alle 5 Sekunden
         // setInterval(() => {
         //     this.loadCurrentValue();
-        // },5000);
+        // }, 5000);
     }
 
     loadCurrentValue() {
@@ -17,4 +18,17 @@ export default class extends Controller {
             });
     }
 
+    toggleFullscreen() {
+        const element = this.element;
+
+        if (!document.fullscreenElement) {
+            element.requestFullscreen().catch(err => {
+                console.error(`Fehler beim Aktivieren des Vollbildmodus: ${err.message}`);
+            });
+        } else {
+            document.exitFullscreen().catch(err => {
+                console.error(`Fehler beim Beenden des Vollbildmodus: ${err.message}`);
+            });
+        }
+    }
 }
